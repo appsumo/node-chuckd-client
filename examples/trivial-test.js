a721e45231d3124377202b1410766d6c079f971f
Damian Hodgkiss <damian@hodgkiss.id.au>
2015-05-21 08:05:53 +0930

    more tests

--- examples/trivial-test.js
+++ examples/trivial-test.js
@@ -8,3 +8,7 @@ var campaignId = 'campaignId';
 CDC.put('a25489e9-848c-4356-97ff-161b0852c509', siteId, pathHash + ':' + campaignId, 'Percent 50', 5);
 CDC.increment('a25489e9-848c-4356-97ff-161b0852c509', siteId, pathHash + ':' + campaignId, 'Percent 50', 1);
 CDC.decrement('a25489e9-848c-4356-97ff-161b0852c509', siteId, pathHash + ':' + campaignId, 'Percent 50', 2);
+
+CDC.increment('a25489e9-848c-4356-97ff-161b0852c509', siteId, pathHash + ':' + campaignId, 'Percent 40', 1);
+
+CDC.decrement('a25489e9-848c-4356-97ff-161b0852c509', siteId, pathHash + ':' + campaignId, 'Percent 60', 2);
